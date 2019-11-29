0d576d8fb6e48d8bf2d41fb714832ecf4a8a8324
luisan00 <luisan00@hotmail.com>
2019-11-29 19:39:18 +0100

    building basic structure

---
+++ fuelgauge/src/main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file main.cpp
+ * @author Locha Mesh Developers (locha.io.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-11-29
+ * 
+ * @copyright Copyright (c) 2019 Locha Mesh
+ * 
+ */
