5a5ec6013b72919ca53e6c8aff5881bf884cd13f
luisan00 <luisan00@hotmail.com>
2019-12-15 21:40:55 +0100

    Create structure for SPI screen

---
+++ screen/main/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Locha Mesh Developers (locha.io)
+ * @brief 
+ * @version 0.1
+ * @date 2019-12-15
+ * 
+ * @copyright Copyright (c) 2019 Locha Mesh
+ * 
+ */
+
+extern "C" {
+void app_main(void);
+}
+
+void app_main()
+{
+    //
+}
